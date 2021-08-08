628d0c2202ca6b770c1bd098c0865540499df082
Oh <oss0202@naver.com>
2021-08-08 15:35:44 +0900

    지네릭 메서드

--- src/generics/Box.java
+++ src/generics/Box.java
@@ -1,5 +1,7 @@
 package generics;
 
+import java.util.List;
+
 public class Box<T extends String> {// 지네릭 타입 T를 선언
     T item;
 
@@ -8,5 +10,8 @@ public class Box<T extends String> {// 지네릭 타입 T를 선언
     }
     T getItem(){
         return item;
+    }
+    static <T> void sort(List<T> list, Comparable<? super T> c){
+
     }
 }
