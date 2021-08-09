48bc5d44a859286de6e1d7e11af6f44b7bafd54a
오성식 <oss0202@etoos.com>
2021-08-09 10:21:24 +0900

    쓰레드 우선순위

---
+++ src/thread/ThreadEx8.java
@@ -0,0 +1,16 @@
+package thread;
+
+public class ThreadEx8 {
+    public static void main(String[] args) {
+        ThreadEx8_1 th1 = new ThreadEx8_1();
+        ThreadEx8_2 th2 = new ThreadEx8_2();
+
+        th2.setPriority(7);
+
+        System.out.println("th1(-) : " + th1.getPriority());
+        System.out.println("th2(|) : " + th2.getPriority());
+
+        th1.start();
+        th2.start();
+    }
+}
