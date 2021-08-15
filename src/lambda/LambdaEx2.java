f85249e1b94c857fdebe0f79d25a8eb4ed245db2
오성식 <oss0202@naver.com>
2021-08-16 01:01:13 +0900

    Functional defautl, static method 추가

--- src/lambda/LambdaEx2.java
+++ src/lambda/LambdaEx2.java
@@ -12,6 +12,7 @@ public class LambdaEx2 {
         System.out.println("anonymous function - big : " + big);
 
         myFunction = (int a, int b) -> a > b ? a : b;
+        myFunction = (a, b) -> a > b ? a : b;
         myFunction.max(2,5);
         System.out.println("lambda - big : " + big);
     }
--- src/lambda/MyFunction.java
+++ src/lambda/MyFunction.java
@@ -2,5 +2,11 @@ package lambda;
 
 @FunctionalInterface
 public interface MyFunction {
-    public abstract int max(int a, int b);
+    int max(int a, int b);
+    default void maxMethod(int a, int b){
+
+    }
+    static int maxstatic(){
+        return 1;
+    }
 }
