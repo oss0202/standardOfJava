76babe4740084e0b2830e858d6643a1422d37cd2
oss0202 <oss0202@naver.com>
2021-09-01 13:10:37 +0900

    Date set test

---
+++ src/localdatetime/DateTimeEx1.java
@@ -0,0 +1,11 @@
+package localdatetime;
+
+import java.util.Date;
+
+public class DateTimeEx1 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        Date date2 = new Date();
+        date.setTime(11L);
+    }
+}
