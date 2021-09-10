e87c069d220ce943a67996a4f1a2d6192122663a
오성식 <oss0202@naver.com>
2021-09-10 20:59:55 +0900

    for break test

--- src/ArrayCopyTest.java
+++ src/ArrayCopyTest.java
@@ -10,8 +10,40 @@ public class ArrayCopyTest {
         String[] copyTo = new String[7];
         System.arraycopy(copyFrom, 2, copyTo, 0, 7);
         copyFrom[0] = "sss";
-        Arrays.stream(copyTo).forEach(System.out::println);
-        System.out.println("-----------------------------");
-        Arrays.stream(copyFrom).forEach(System.out::println);
+//        Arrays.stream(copyTo).forEach(System.out::println);
+//        System.out.println("-----------------------------");
+//        Arrays.stream(copyFrom).forEach(System.out::println);
+
+        String searchMe = "Look for a substring in me";
+        String substring = "sub";
+        boolean foundIt = false;
+
+        int max = searchMe.length() -
+                substring.length();
+
+        String str = "";
+//            System.out.println(searchMe.length());
+//            System.out.println(max);
+        test:
+        for (int i = 0; i <= max; i++) {
+            int n = substring.length();
+            int j = i;
+            int k = 0;
+
+            while (n-- != 0) {
+                if (searchMe.charAt(j++) != substring.charAt(k++)) {
+                    continue test;
+                }else{
+                    str = j + ", " + k;
+                }
+            }
+            foundIt = true;
+            System.out.println(i);
+            break test;
+        }
+//        System.out.println(str);
+        System.out.println(foundIt ? "Found it" : "Didn't find it");
+
+//        System.out.println(searchMe.charAt(14));
     }
 }
