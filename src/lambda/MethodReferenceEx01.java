dd1530ab6979336840f81162753ba49a2b3786bd
오성식 <oss0202@naver.com>
2021-09-11 20:22:50 +0900

    Method References

--- src/lambda/MethodReferenceEx01.java
+++ src/lambda/MethodReferenceEx01.java
@@ -12,12 +12,12 @@ public class MethodReferenceEx01 {
         //2. 인스턴스메서드 참조
         BiFunction<String, String, Boolean> f3 = (s1, s2) -> s1.equals(s2);
         BiFunction<String, String, Boolean> f4 =String::equals;
+        Function<String, Boolean> f7 = MethodReferenceEx01::sample;//메서드 참조
 
         //3. 특정 객체 인스턴스메서드 참조
         MethodReferenceEx01 obj = new MethodReferenceEx01();
         Function<String, Boolean> f5 = (x) -> obj.equals(obj);//람다식
         Function<String, Boolean> f6 = obj::equals;//메서드 참조
-        Function<String, Boolean> f7 = MethodReferenceEx01::sample;//메서드 참조
 
     }
     public static boolean sample(String sample){
