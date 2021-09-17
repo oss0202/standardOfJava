662cba4e863f52384592385caf2df580fe0f108a
오성식 <oss0202@naver.com>
2021-09-17 21:09:03 +0900

    Inheritance Test

--- src/main/java/Bicycle.java
+++ src/main/java/Bicycle.java
@@ -1,49 +1,23 @@
 public class Bicycle {
-    private int cadence;
-    private int gear;
-    private int speed;
+    // the Bicycle class has three fields
+    public int cadence;
+    public int gear;
+    public int speed;
 
-    private int id;
-
-    private static int numberOfBicycles = 0;
-
-
-    public Bicycle(int startCadence,
-                   int startSpeed,
-                   int startGear) {
+    // the Bicycle class has one constructor
+    public Bicycle(int startCadence, int startSpeed, int startGear) {
         gear = startGear;
         cadence = startCadence;
         speed = startSpeed;
-
-        id = ++numberOfBicycles;
     }
 
-    public int getID() {
-        return id;
-    }
-
-    public static int getNumberOfBicycles() {
-        return numberOfBicycles;
-    }
-
-    public int getCadence() {
-        return cadence;
-    }
-
+    // the Bicycle class has four methods
     public void setCadence(int newValue) {
         cadence = newValue;
     }
 
-    public int getGear(){
-        return gear;
-    }
-
     public void setGear(int newValue) {
         gear = newValue;
-    }
-
-    public int getSpeed() {
-        return speed;
     }
 
     public void applyBrake(int decrement) {
