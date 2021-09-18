83aa7c89de119580f3f3de7dcfbcaf295a589b7a
오성식 <oss0202@naver.com>
2021-09-18 21:40:56 +0900

    Error Remove

--- src/main/java/UseBicycle.java
+++ src/main/java/UseBicycle.java
@@ -1,13 +1,13 @@
 public class UseBicycle {
     public static void main(String[] args) {
         Bicycle bicycle1 = new Bicycle(1,1,1);
-        System.out.println("numberOfBicycles : " + Bicycle.getNumberOfBicycles());
+//        System.out.println("numberOfBicycles : " + Bicycle.getNumberOfBicycles());
         Bicycle bicycle2 = new Bicycle(2,2,2);
-        System.out.println("numberOfBicycles : " + Bicycle.getNumberOfBicycles());
+//        System.out.println("numberOfBicycles : " + Bicycle.getNumberOfBicycles());
         Bicycle bicycle3 = new Bicycle(3,3,3);
-        System.out.println("numberOfBicycles : " + Bicycle.getNumberOfBicycles());
+//        System.out.println("numberOfBicycles : " + Bicycle.getNumberOfBicycles());
 
         System.out.println("---------------");
-        System.out.println(bicycle1.getNumberOfBicycles());
+//        System.out.println(bicycle1.getNumberOfBicycles());
     }
 }
