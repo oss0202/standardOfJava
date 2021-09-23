8faba7b851af12eb0c178fb1e8d811cde5b0d8b6
오성식 <oss0202@naver.com>
2021-09-23 21:07:31 +0900

    Bounded Type Parameters

--- src/main/java/tutorial/Box.java
+++ src/main/java/tutorial/Box.java
@@ -10,4 +10,16 @@ public class Box<T> {
 
     public void set(T t) { this.t = t; }
     public T get() { return t; }
+
+    public <U extends Number> void inspect(U u){
+        System.out.println("T: " + t.getClass().getName());
+        System.out.println("U: " + u.getClass().getName());
+    }
+
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<Integer>();
+        integerBox.set(new Integer(10));
+        integerBox.inspect(10); // error: this is still String!
+//        integerBox.inspect("some text"); // error: this is still String!
+    }
 }
