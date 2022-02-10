2afd40a687df3cab03f9357a5c99c35727f3462f
오성식 <oss0202@naver.com>
2022-02-10 13:04:40 +0900

    intellij call stack debugging test

--- src/main/java/LongTest.java
+++ src/main/java/LongTest.java
@@ -1,7 +1,25 @@
 public class LongTest {
     public static void main(String[] args) {
-        Long aLong = null;
-        System.out.println(aLong);
-        System.out.println(aLong==null);
+        firstMethodCall();
+    }
+
+    private static void fourthMethodCall() {
+        System.out.println("LongTest.fourthMethodCall");
+
+    }
+
+    private static void thirdMethodCall() {
+        System.out.println("LongTest.thirdMethodCall");
+        fourthMethodCall();
+    }
+
+    private static void secondMethodCall() {
+        System.out.println("LongTest.secondMethodCall");
+        thirdMethodCall();
+    }
+
+    private static void firstMethodCall() {
+        System.out.println("LongTest.firstMethodCall");
+        secondMethodCall();
     }
 }
