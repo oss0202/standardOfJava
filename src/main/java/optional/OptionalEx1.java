bf40bf5dfb8cad8fb4707a101192b30ea5f78a74
오성식 <oss0202@naver.com>
2022-02-15 17:16:16 +0900

    Java Optional Update

--- src/main/java/optional/OptionalEx1.java
+++ src/main/java/optional/OptionalEx1.java
@@ -5,13 +5,18 @@ import java.util.Optional;
 
 public class OptionalEx1 {
     public static void main(String[] args) {
-        Optional<Payment> payment = Optional.ofNullable(new Payment(11L, "1234", LocalDateTime.now(), LocalDateTime.now()));
-        payment.map(payObj -> {
-            LocalDateTime ordDateTime = payObj.getInputDt();
+        Optional<Payment> optPayment = Optional.ofNullable(new Payment(11L, "1234", LocalDateTime.now(), LocalDateTime.now()));
+        optPayment.map(payObj -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LocalDateTime ordDateTime = LocalDateTime.now();//payObj.getInputDt();
             payObj.setOrderTime(ordDateTime);
             return payObj;
-        }).ifPresent(payObj -> {
-            System.out.println(payObj.toString());
-        });
+        }).ifPresent(System.out::println);
+
+        Optional.ofNullable(optPayment);
     }
 }
