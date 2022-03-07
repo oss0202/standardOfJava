ee079d89d2a0d7b59e052bf699d3e39a72d85652
오성식 <oss0202@naver.com>
2022-03-07 20:16:19 +0900

    학생들의 점수배열을 리턴(멀티 쓰레드 사용)

--- src/main/java/stream/StreamEx20.java
+++ src/main/java/stream/StreamEx20.java
@@ -14,4 +14,15 @@ public class StreamEx20 {
 				.map(myStudent -> myStudent.getScore())
 				.collect(Collectors.toList());
 	}
+
+	/**
+	 * 학생들의 점수배열을 리턴(멀티 쓰레드 사용)
+	 * @param myStudentList
+	 * @return
+	 */
+	public List<Integer> getScoresParallel(List<MyStudent> myStudentList){
+		return myStudentList.parallelStream()
+				.map(myStudent -> myStudent.getScore())
+				.collect(Collectors.toList());
+	}
 }
