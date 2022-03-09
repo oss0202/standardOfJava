fb54f579071499f0fe66422cc92c9e57441baa4a
오성식 <oss0202@naver.com>
2022-03-09 20:16:19 +0900

    내부반복 및 오타수정

--- src/main/java/stream/IteratorEx01.java
+++ src/main/java/stream/IteratorEx01.java
@@ -3,16 +3,24 @@ package stream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class IteratorEx01 {
-    public void externalIterator(List<MyStudent> menu){
+    public List<String> externalIterator(List<MyStudent> myStudentList){
         List<String> highScoreStudentNm = new ArrayList<>();
-        Iterator<MyStudent> iterator = menu.iterator();
+        Iterator<MyStudent> iterator = myStudentList.iterator();
         while (iterator.hasNext()){
             MyStudent myStudent = iterator.next();
             if(myStudent.getScore() > 70){
                 highScoreStudentNm.add(myStudent.getName());
             }
         }
+        return highScoreStudentNm;
+    }
+    public List<String> internalIterator(List<MyStudent> myStudentList){
+        return myStudentList.stream()
+                .filter(student -> student.getScore() > 70)
+                .map(MyStudent::getName)
+                .collect(Collectors.toList());
     }
 }
