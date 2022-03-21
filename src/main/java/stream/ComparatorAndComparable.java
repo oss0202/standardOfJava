7aa14b7aa1edf9143e90db5992df60440d1f11ba
오성식 <oss0202@naver.com>
2022-03-21 21:04:12 +0900

    어떻게 두 학생을 비교할 것인가?

---
+++ src/main/java/stream/ComparatorAndComparable.java
@@ -0,0 +1,16 @@
+package stream;
+
+/**
+ * @author : 221213
+ * @date : 2022. 03. 14
+ */
+public class ComparatorAndComparable {
+    public static void main(String[] args) {
+        Student student1 = new Student("첫번째", 1, 240);
+        Student student2 = new Student("두번째", 2, 300);
+        /**
+         * 어떻게 두 학생을 비교할 것인가?
+         *  if (student1 > student2) .... ?
+         */
+    }
+}
